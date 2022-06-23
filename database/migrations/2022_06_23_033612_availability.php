6ff3b3887e13a58d34a230c289ff813380c5c0c5
Mehul Singh Rathore <mehulsingh072001@gmail.com>
2022-06-23 09:09:55 +0530

    added availability model too

---
+++ database/migrations/2022_06_23_033612_availability.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('availability', function(Blueprint $table){
+            $table->id();
+            $table->string('name');
+            $table->json('rules');
+            $table->string('timezone');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
