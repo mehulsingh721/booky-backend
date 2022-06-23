18c69035386eae8846f2fbd80a8817ec68e7dfdd
Mehul Singh Rathore <mehulsingh072001@gmail.com>
2022-06-23 14:37:39 +0530

    solved foreign key problem

--- app/Models/AppUserEvent.php
+++ app/Models/AppUserEvent.php
@@ -21,6 +21,6 @@ class AppUserEvent extends Model
 
     public function user()
     {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class, "user_id");
     }
 }
